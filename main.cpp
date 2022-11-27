9ea825bc7bddff258e7d2b9dc153a575ac5642a5
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-11-27 20:49:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main() {
-int a, b, c, i, n;
-cin >> n;
-a = 1;
-b = 1;
-cout << a << b << endl;
-for (i = 0; i<=n; i = i+a) {
-c = a + b;
-a = b;
-b = c;
-cout << c << endl;
-}
+   int n=0, a=1, b=1, c=0;
+   cin >> n;
+   cout << a << endl << b << endl;
+   for (int i=0; i <= n; i++){
+     c= a+b;
+      a= b;
+      b= c;
+      cout << c << endl;
+   }
+   return 0;
 }
